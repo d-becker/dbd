dbf49bc4bd9df2795cc8992537b93267394f70c4
Daniel Becker <daniel.becer@cloudera.com>
2018-07-26 15:06:59 +0200

    Oozie-5 now works.

--- resources/oozie/scripts/start_oozie_example.sh
+++ resources/oozie/scripts/start_oozie_example.sh
@@ -1,1 +1,1 @@
-bin/oozie job -config examples/apps/java-main/job.properties -run -DnameNode=hdfs://namenode:9000 -DjobTracker=resourcemanager:8032
+bin/oozie job -config examples/apps/java-main/job.properties -run -DnameNode=hdfs://namenode:9000 -DjobTracker=resourcemanager:8032 -DresourceManager=resourcemanager:8032
