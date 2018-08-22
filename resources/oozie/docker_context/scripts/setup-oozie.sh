97f2831c9f7590334db6c0ee0009b1a1f9b8854d
Daniel Becker <daniel.becker@cloudera.com>
2018-08-22 12:31:06 +0200

    Some shellcheck.

--- resources/oozie/docker_context/scripts/setup-oozie.sh
+++ resources/oozie/docker_context/scripts/setup-oozie.sh
@@ -1,35 +1,38 @@
+#!/bin/bash
+
 python3 scripts/xmlcombine.py /opt/oozie/conf/hadoop-conf/core-site.xml /opt/hadoop/etc/hadoop/core-site.xml > tmp.xml
 mv tmp.xml /opt/oozie/conf/hadoop-conf/core-site.xml
 
-echo "Successfully copied the contents of the Hadoop core-site.xml to the Oozie configuration." > docker_oozie_logs.txt
+function log {
+        local message=$1
+        echo "$message" >> docker_oozie_logs.txt
+}
+
+log "Successfully copied the contents of the Hadoop core-site.xml to the Oozie configuration."
 
 while [ "$(hdfs dfsadmin -report)" = "" ]
 do
     sleep 0.5
 done
 
-sudo -u hadoop JAVA_HOME=$JAVA_HOME hdfs dfs -mkdir /user
-sudo -u hadoop JAVA_HOME=$JAVA_HOME hdfs dfs -mkdir /user/oozie
-sudo -u hadoop JAVA_HOME=$JAVA_HOME hdfs dfs -chown -R oozie:oozie /user/oozie
-sudo -u hadoop JAVA_HOME=$JAVA_HOME hdfs dfs -chmod -R a+w /
+sudo -u hadoop JAVA_HOME="$JAVA_HOME" hdfs dfs -mkdir /user
+sudo -u hadoop JAVA_HOME="$JAVA_HOME" hdfs dfs -mkdir /user/oozie
+sudo -u hadoop JAVA_HOME="$JAVA_HOME" hdfs dfs -chown -R oozie:oozie /user/oozie
+sudo -u hadoop JAVA_HOME="$JAVA_HOME" hdfs dfs -chmod -R a+w /
 
-echo "Successfully set up hdfs user directory." >> docker_oozie_logs.txt
+log "Successfully set up hdfs user directory."
 
 /opt/oozie/bin/oozie-setup.sh sharelib create -fs hdfs://namenode:9000
 
-echo "Successfully uploaded the sharelib." >> docker_oozie_logs.txt
+log "Successfully uploaded the sharelib."
 
-sudo -u hadoop JAVA_HOME=$JAVA_HOME hdfs dfs -chmod -R a+rwx /
+sudo -u hadoop JAVA_HOME="$JAVA_HOME" hdfs dfs -chmod -R a+rwx /
 
-echo "Write permissions on hdfs." >> docker_oozie_logs.txt
+log "Write permissions on hdfs."
 
-/opt/oozie/bin/oozied.sh start
-
-if [ $? -eq 0 ]
+if /opt/oozie/bin/oozied.sh start
 then
-    echo "Started the Oozie server." >> docker_oozie_logs.txt 
+    log "Started the Oozie server."
 else
-    echo "Could not start the Oozie server." >> docker_oozie_logs.txt
+    log "Could not start the Oozie server."
 fi
-
-/bin/bash
--- resources/spark/docker_context/init.sh
+++ resources/spark/docker_context/init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 while [ "$(hdfs dfsadmin -report)" = "" ]
 do
     sleep 0.5
@@ -7,6 +9,7 @@ hdfs dfs -mkdir /sparklog
 hdfs dfs -chmod 777 /sparklog
 hdfs dfs -chmod 777 /
 
-export SPARK_DIST_CLASSPATH=$(hadoop classpath)
+SPARK_DIST_CLASSPATH=$(hadoop classpath)
+export SPARK_DIST_CLASSPATH
 
 bin/spark-class org.apache.spark.deploy.history.HistoryServer
